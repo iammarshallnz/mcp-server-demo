72e51be2abf170c9237e62f8acfb49a842cced57
iammarshallnz <marshallinch@gmail.com>
2025-07-30 11:05:35 +1200

    Better read me and edits to main.py

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 An MCP that connects to my desktop to scan for files, then uses a resume file and a job description file to create a new laTex file of my resume.
 
-- I'm not sure if this is done well
-- It needs some changes 
-- It kind of works 
+- This is a simple mcp that exposes the users desktop to claude allowing reading and writting of files in a standadised way
 
-To install in claude
+Install py mcp, and uv (Python package and project manager)
 ```bash
-mcp install main.py
+pip install mcp
+pip install uv
+```
+
+To install (connect) in claude
+```bash
+uv run mcp install main.py
 ```
 
 Alternatively, you can test it with the MCP Inspector:
 ```bash
-mcp dev main.py
+uv run mcp dev main.py
 ```
--- main.py
+++ main.py
@@ -2,24 +2,8 @@
 # server.py
 import os
 from mcp.server.fastmcp import FastMCP
-from mcp import types
-import mcp.types
 # Create an MCP server
 mcp = FastMCP("Demo")
-
-
-# Add an addition tool
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
-
-
-# Add a dynamic greeting resource
-@mcp.resource("greeting://{name}")
-def get_greeting(name: str) -> str:
-    """Get a personalized greeting"""
-    return f"Hello, {name}!"
 
 
 @mcp.tool(
@@ -29,7 +13,8 @@ def get_greeting(name: str) -> str:
 async def readLaTex(fileName: str) -> str:
     """Read a LaTex file and return its content"""
     # open files in desktop
-    url = rf'C:\Users\YABOIMARSH\Desktop\{fileName}'
+    username = os.getlogin()    # Fetch usernam
+    url = rf'C:\Users\{username}\Desktop\{fileName}'
     try:
         with open(url, "r") as doc:
             content = []
@@ -47,7 +32,8 @@ async def readLaTex(fileName: str) -> str:
 async def writeLaTex(fileName: str, content: str) -> None:
     """Write content to a NEW LaTex file"""
     # open files in desktop
-    url = rf"C:\Users\YABOIMARSH\Desktop\{fileName}"
+    username = os.getlogin()    # Fetch usernam
+    url = rf'C:\Users\{username}\Desktop\{fileName}'
     doc = open(url, "w")
     doc.write(content)
     doc.close()
@@ -60,7 +46,8 @@ async def writeLaTex(fileName: str, content: str) -> None:
 async def readTxtFile(fileName: str) -> str:
     """Read a text file and return its content"""
     # open files in desktop
-    url = rf'C:\Users\YABOIMARSH\Desktop\{fileName}'
+    username = os.getlogin()    # Fetch usernam
+    url = rf'C:\Users\{username}\Desktop\{fileName}'
     try:
         with open(url, "r") as doc:
             content = []
@@ -72,30 +59,38 @@ async def readTxtFile(fileName: str) -> str:
     return "\n".join(content)
 
 
-@mcp.resource("desktop://data")
-def getData() -> str:
+@mcp.tool(
+    name="read-desktop",
+    description="reads the desktop",
+) 
+def read_users_desktop_tool() -> str:
+    try:
+        username = os.getlogin()    # Fetch usernam
+        url = rf'C:\Users\{username}\Desktop'
+        files = os.listdir(url)
+        return str({"files": files})
+    except Exception as e:
+        return str({"error": str(e)})
+
+
+@mcp.resource(
+    uri ="desktop://data",
+    name='read-desktop-data',
+    description='Connects to the users desktop and reads name of all the files'
+    )
+def read_desktop_data() -> str:
     """desktop data """
     try:
-        files = os.listdir(r'C:\Users\YABOIMARSH\Desktop\\')
+        username = os.getlogin()    # Fetch usernam
+        url = rf'C:\Users\{username}\Desktop'
+        files = os.listdir(url)
         return str({"files": files})
     except Exception as e:
         return str({"error": str(e)})
 
 
 
-@mcp.resource("config://app")
-def get_config() -> str:
-    """Static configuration data"""
-    return "App configuration here"
-
-
-@mcp.resource("users://{user_id}/profile")
-def get_user_profile(user_id: str) -> str:
-    """Dynamic user data"""
-    return f"Profile data for user {user_id}"
-"""Dont scrape i got banned from indeed"""
-
-
 if __name__ == "__main__":
+
     mcp.run(transport="stdio")
-    
+     
